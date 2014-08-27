971d88491e5a4597cd22cbd5edd00cfa1cc94847
spoonkojo <spoonkojo@gmail.com>
2014-08-26 19:13:53 -0600

    add modal with selectize

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,8 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'selectize-ng'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/identities', {templateUrl: 'partials/identities.html', controller: 'IdentitiesCtrl'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,10 +16,32 @@ angular.module('myApp.controllers', [])
     };
 
     $scope.getIdentities();
+
   }])
   .controller('ProjectsCtrl', ['$scope', function($scope) {
 
   }])
   .controller('TagsCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('AddIdentityModalCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.tags = [
+      { "id": 1, "name": "webadmin" },
+      { "id": 2, "name": "cpanel" },
+      { "id": 3, "name": "mysql" }
+    ];
+
+    $scope.tagging = {
+      value: [1],
+      options: {
+        valueField: 'id',
+        labelField: 'name',
+        searchField: ['name'],
+        create: true
+      },
+    };
+
+    $scope.tag = $scope.tags[0];
 
   }]);
